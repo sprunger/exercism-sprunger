533f005dbbb82064b8b2d33a3cdcefb5e9dbab48
sprunger <scott@globaleworks.com>
2014-10-01 15:59:22 -0400

    First pass of Raindrops, crude but all passing.

---
+++ ruby/raindrops/raindrops.rb
@@ -0,0 +1,10 @@
+class Raindrops
+  def self.convert(value)
+    result = ''
+    result << 'Pling' if value % 3 == 0
+    result << 'Plang' if value % 5 == 0
+    result << 'Plong' if value % 7 == 0
+
+    result.empty? ? value.to_s : result
+  end
+end
