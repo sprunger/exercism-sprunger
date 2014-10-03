72166c0f2bd9bda11f63cd14c223618fda5945eb
sprunger <scott@globaleworks.com>
2014-10-03 11:00:38 -0400

    A little more refactoring

--- ruby/robot-name/robot.rb
+++ ruby/robot-name/robot.rb
@@ -7,17 +7,17 @@ class Robot
   end
 
   def reset
-    @name = random_string(2) + random_number(3)
+    @name = random_string + random_number
   end
 
   private
 
-  def random_string(length=10)
-    select_random(2, 'abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ')
+  def random_string(length=2)
+    select_random(length, 'abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ')
  end
 
-  def random_number(length=10)
-    select_random(3, '0123456789')
+  def random_number(length=3)
+    select_random(length, '0123456789')
   end
 
   def select_random(length=10, chars="")
