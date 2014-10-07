1fdf38565dd518325d3cf64e3f1ab2e27daec73c
sprunger <scott@globaleworks.com>
2014-10-07 19:37:16 -0400

    Much better word parsing with scan

--- ruby/word-count/phrase.rb
+++ ruby/word-count/phrase.rb
@@ -1,14 +1,12 @@
 class Phrase
 
   def initialize(phrase)
-    @phrase = phrase.
-      gsub(/[,]/i, ' ').
-      gsub(/[^0-9a-z ']/i, '').downcase
+    @words = phrase.downcase.scan(/[\w']+/)
   end
 
   def word_count
     words = Hash.new(0)
-    @phrase.split.each { |w| words[w] += 1 }
+    @words.each { |w| words[w] += 1 }
     words
   end
 
