cd0f405336c6b659e77c673ed090a10ba7a41d8a
sprunger <scott@globaleworks.com>
2014-10-07 13:33:05 -0400

    Define squares on the board, not the magic number

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,16 +1,15 @@
 class Grains
 
   def initialize
+    @TOTAL_SQUARES = 64
   end
 
   def square(n)
     2 ** (n - 1)
-    #1 << ( n - 1 )
   end
 
   def total
-    2 ** (64) - 1
-    #(1 << 64) - 1
+    2 ** (@TOTAL_SQUARES) - 1
   end
 
 end
