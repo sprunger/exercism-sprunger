746b92b81a33dcc15475cc11770a50ef231c37db
sprunger <scott@globaleworks.com>
2014-10-08 19:41:53 -0400

    Oh my.  Python list comprehensions.

--- python/word-count/wordcount.py
+++ python/word-count/wordcount.py
@@ -1,9 +1,13 @@
 import re
 
 def word_count(phrase):
-  words = {}
+  words = phrase.split()
+  return { item: words.count(item) for item in words }
 
-  for word in phrase.split() :
-    words[word] = len(re.findall(r'(\b|\s)%s(\b|\s|$)' % re.escape(word), phrase))
 
-  return words
+  #words = {}
+
+  #for word in phrase.split() :
+  #  words[word] = len(re.findall(r'(\b|\s)%s(\b|\s|$)' % re.escape(word), phrase))
+
+  #return words
