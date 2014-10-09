c5b48a4d6d75a08096b7aebca582b770539f8141
sprunger <scott@globaleworks.com>
2014-10-08 23:43:34 -0400

    Format and tidy up

--- python/rna-transcription/dna.py
+++ python/rna-transcription/dna.py
@@ -1,6 +1,12 @@
 def to_rna(strand):
 
-  strand_map = {'G': 'C', 'C': 'G', 'T': 'A', 'A': 'U'}
+  strand_map = {
+    'G': 'C',
+    'C': 'G',
+    'T': 'A',
+    'A': 'U'
+  }
+
   return ''.join([strand_map[c] for c in strand])
 
   ''' if you want to import string or are using Python 3.x much simpler
