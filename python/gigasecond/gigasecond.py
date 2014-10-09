a1cf6105142c0e4941d7a2e267bf7bb5b22f0ee2
sprunger <scott@globaleworks.com>
2014-10-09 18:24:18 -0400

    Learned more about timedelta

--- python/gigasecond/gigasecond.py
+++ python/gigasecond/gigasecond.py
@@ -3,7 +3,5 @@ from datetime import timedelta
 def add_gigasecond(start_date):
 
   GIGASECOND = 10**9
-  SECONDS_PER_DAY = 86400
-  DAYS_IN_A_GIGASECOND = GIGASECOND / SECONDS_PER_DAY
 
-  return start_date + timedelta(DAYS_IN_A_GIGASECOND)
+  return start_date + timedelta(seconds=GIGASECOND)
