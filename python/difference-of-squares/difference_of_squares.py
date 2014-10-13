b276b84b248d2a4512e7bdcac64d6e58d15ddcfa
sprunger <scott@globaleworks.com>
2014-10-13 19:55:45 -0400

    Exercism squares in Python

---
+++ python/difference-of-squares/difference_of_squares.py
@@ -0,0 +1,9 @@
+def square_of_sum(value):
+  return sum(x for x in range(value+1)) ** 2
+
+def sum_of_squares(value):
+  return sum(x**2 for x in range(value+1))
+
+def difference(value):
+  return square_of_sum(value) - sum_of_squares(value)
+
