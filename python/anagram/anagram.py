a7c4c92f0e645f97c9c76c8cf4b33888e658c38a
sprunger <scott@globaleworks.com>
2014-10-14 10:42:59 -0400

    Remove temporary variables and just compare the sorted lists.

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -4,12 +4,8 @@ def detect_anagrams(word, candidates):
   # Same word is not an anagram
   candidates = [x for x in candidates if x.lower() != word.lower()]
 
-  letters = sorted(word.lower())
-
   for candidate in candidates:
-    anagram = sorted(candidate.lower())
-    # If the sorted list of letters in two words are the same, presto!
-    if letters == anagram:
+    if sorted(word.lower()) == sorted(candidate.lower()):
       anagrams.append(candidate)
 
   return anagrams
