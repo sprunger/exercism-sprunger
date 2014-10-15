6d4ea95ebc2e96437dcd0200326fd594a3217a71
sprunger <scott@globaleworks.com>
2014-10-15 15:44:35 -0400

    Actually follow rules using True and False.  Managed to refactor a little bit.

--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -1,15 +1,11 @@
 def sieve(limit):
-  candidates = dict.fromkeys(range(2,limit+1), None)
+  candidates = dict.fromkeys(range(2,limit+1), True)
 
   for current in candidates.keys():
     for k in candidates.keys():
-      if candidates[current] == None:
-        candidates[current] = 'Prime'
-      else:
-        if candidates[k] == None:
-          if k % current == 0:
-            candidates[k] = 'Composite'
+      if candidates[k] == False or k == current:
+        continue
+      if k % current == 0:
+        candidates[k] = False
 
-  return [x for x in candidates if candidates[x] == 'Prime']
-
-
+  return [x for x in candidates if candidates[x] == True]
