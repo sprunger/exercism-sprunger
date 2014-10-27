844680755c1c8923abad8699dea011b492917b74
sprunger <scott@globaleworks.com>
2014-10-27 07:51:37 -0400

    Minor refactoring

--- ruby/binary/binary.rb
+++ ruby/binary/binary.rb
@@ -2,21 +2,22 @@ class Binary
   attr_reader :binary
 
   def initialize(value)
-    @binary ||= value.to_i.to_s.scan(/\d/)
+    @binary = [0]
+    @binary = value.scan(/\d/).map(&:to_i) if value =~ /^[0|1]*$/
   end
 
   def to_decimal()
     total = 0
 
     binary.reverse.each_with_index do |n, i|
-      total += n.to_i * 2**i
+      total += n * 2**i
     end
 
     total
   end
 end
 
-# If we could just use the sweetness of Ruby
+# If we could just use the sweetness of Ruby...
 #
 # Class Binary
 #   attr_reader :binary
