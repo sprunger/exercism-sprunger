fa5c37dcf530ec6dc4b4b12d9fe2a765232ebbf0
sprunger <scott@globaleworks.com>
2014-10-27 08:30:38 -0400

    Minor refactor but much cleaner

--- ruby/accumulate/array.rb
+++ ruby/accumulate/array.rb
@@ -1,9 +1,9 @@
 class Array
   def accumulate
-    accumulator = []
-    self.each do |item|
+    #accumulator = []
+    self.each_with_object([]) do |item, accumulator|
       accumulator << yield(item)
     end
-    accumulator
+    #accumulator
   end
 end
