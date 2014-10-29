11e6648cc2849d8ce3d893b5cf463f994eca4d30
sprunger <scott@globaleworks.com>
2014-10-28 20:13:56 -0400

    Exercism phone number exercise in Ruby

---
+++ ruby/phone-number/phone_number.rb
@@ -0,0 +1,46 @@
+class PhoneNumber
+
+  attr_reader :number
+
+  def initialize(phone)
+    @number = parse(phone)
+  end
+
+  def to_s
+     "(#{area_code}) #{exchange}-#{subscriber}"
+  end
+
+  def area_code
+    number.slice(0,3) if valid?
+  end
+
+  private
+
+  def valid?
+    number != '0000000000'
+  end
+
+  def invalid
+    '0'*10
+  end
+
+  def exchange
+    number.slice(3,3)
+  end
+
+  def subscriber
+    number.slice(6,4)
+  end
+
+  def parse(string)
+
+    return invalid if string =~ /[A-Za-z]/
+
+    string.gsub!(/[^0-9]/,"")
+
+    string = string[1..-1] if (string.length > 10 && string[0] == '1')
+    string = invalid if string.length != 10
+    string
+  end
+
+end
