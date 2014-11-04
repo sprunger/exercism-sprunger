979d24b508e38e82b80683f46fc89c0450e449b6
sprunger <scott@globaleworks.com>
2014-11-03 19:01:29 -0500

    Record a solution that I love from another user

--- python/kindergarten-garden/garden.py
+++ python/kindergarten-garden/garden.py
@@ -22,3 +22,22 @@ class Garden:
       )]
 
     return student_plants
+
+# Below is an implementation by @mariazverina
+# I spent several hours trying to figure out how to get a dict with
+# student name and their pots.  This is brilliant.
+#
+# class Garden:
+#     STUDENTS = ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Fred',
+#         'Ginny', 'Harriet', 'Ileana', 'Joseph', 'Kincaid', 'Larry']
+
+#     FLOWERS = {'C': 'Clover', 'R': 'Radishes',
+#         'G': 'Grass', 'V': 'Violets'}
+
+#     def __init__(self, garden, students=STUDENTS):
+#         garden = garden.split()
+#         flowers = zip(*[iter(garden[0])]*2 + [iter(garden[1])]*2)
+#         self.pots = dict(zip(sorted(students), flowers))
+
+#     def plants(self, who):
+#         return map(self.FLOWERS.get, self.pots[who])
