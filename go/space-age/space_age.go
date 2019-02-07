199d0d0eb2415eeb400ae3ee4a8d30c0de4e340e
Scott Sprunger <github@globaleworks.com>
2019-02-07 10:20:01 -0500

    Age in seconds for different planet orbital periods

---
+++ go/space-age/space_age.go
@@ -0,0 +1,25 @@
+package space
+
+type Planet string
+
+// The number of seconds in an Earth year
+var seconds float64 = 31557600
+
+// The number of seconds in planets oribtal period vs. Earth
+var planets = map[string]float64{
+	"Earth":   1.0 * seconds,
+	"Mercury": 0.2408467 * seconds,
+	"Venus":   0.61519726 * seconds,
+	"Mars":    1.8808158 * seconds,
+	"Jupiter": 11.862615 * seconds,
+	"Saturn":  29.447498 * seconds,
+	"Uranus":  84.016846 * seconds,
+	"Neptune": 164.79132 * seconds,
+}
+
+// Given an age in seconds calculate how many years
+// old a person would be for a given planet based on
+// the planets orbital period.
+func Age(age float64, planet Planet) float64 {
+	return age / planets[string(planet)]
+}
