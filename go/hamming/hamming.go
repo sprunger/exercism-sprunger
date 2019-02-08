20dbf3daf6ed08c1c8070d2479548f14584cb763
Scott Sprunger <github@globaleworks.com>
2019-02-08 15:45:47 -0500

    Changes per mentor feedback

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -5,13 +5,13 @@ import "fmt"
 // Distance will calculate the Hamming Distance between two DNA strands
 // Strands must be of equal length
 func Distance(a, b string) (int, error) {
-	var distance int;
-
 	// Could short circuit zero length but optimzation isn't really needed
 
 	if len(a) != len(b) {
-		return -1, fmt.Errorf("hamming: DNA strings must be of equal length")
+		return 0, fmt.Errorf("hamming: DNA strings must be of equal length")
 	}
+
+	var distance int
 
 	for i := range a {
 		if a[i] != b[i] {
