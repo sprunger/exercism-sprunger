d55267108c6e0f2047995fec54c94813edfb9662
Scott Sprunger <github@globaleworks.com>
2019-02-08 15:37:48 -0500

    Proper comment according to golint

--- go/hello-world/hello_world.go
+++ go/hello-world/hello_world.go
@@ -2,7 +2,7 @@
 // https://golang.org/doc/effective_go.html#commentary
 package greeting
 
-// Return the string "Hello, World!"
+// HelloWorld Return the string "Hello, World!"
 func HelloWorld() string {
 	return "Hello, World!"
 }
