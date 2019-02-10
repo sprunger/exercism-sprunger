3d51f0fd09fe541af0b0e63a9f381bc24081f5a3
Scott Sprunger <github@globaleworks.com>
2019-02-10 16:32:32 -0500

    Decided on the single conditional

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -2,17 +2,8 @@ package leap
 
 // IsLeapYear is the year argument a leap year
 func IsLeapYear(year int) bool {
-	// Not sure of idiomatic go - is it a one line conditional
-	// or nested statements?
-
-//  if year%4 == 0 && (year%400 == 0 || year%100 != 0) {
-//    return true
-//  }
-
-	if year%4 == 0 {
-		if year%400 == 0 || year%100 != 0 {
-			return true
-		}
+	if year%4 == 0 && (year%400 == 0 || year%100 != 0) {
+		return true
 	}
 
 	return false
