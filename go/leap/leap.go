8e67613987ea8cb13555e1ffbe9ee0150a0adc04
Scott Sprunger <github@globaleworks.com>
2019-02-14 15:33:56 -0500

    Breakdown conditionals per mentor

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -2,8 +2,10 @@ package leap
 
 // IsLeapYear is the year argument a leap year
 func IsLeapYear(year int) bool {
-	if year%4 == 0 && (year%400 == 0 || year%100 != 0) {
-		return true
+	if year%4 == 0 {
+		if year%400 == 0 || year%100 != 0 {
+			return true
+		}
 	}
 
 	return false
