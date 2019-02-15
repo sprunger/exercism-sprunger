baed1996f2650ef64797b502a26a5b60f0f82607
Scott Sprunger <github@globaleworks.com>
2019-02-15 07:00:17 -0500

    idiomatic Go uses 'early returns'

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -2,10 +2,12 @@ package leap
 
 // IsLeapYear is the year argument a leap year
 func IsLeapYear(year int) bool {
-	if year%4 == 0 {
-		if year%400 == 0 || year%100 != 0 {
-			return true
-		}
+	if year%4 != 0 {
+		return false
+	}
+
+	if year%400 == 0 || year%100 != 0 {
+		return true
 	}
 
 	return false
