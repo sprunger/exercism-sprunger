bf8f864f769466b10757ad482e05a0f97f15a8c4
Scott Sprunger <github@globaleworks.com>
2019-02-15 07:16:42 -0500

    Use constants for proverb text lines

--- go/proverb/proverb.go
+++ go/proverb/proverb.go
@@ -2,6 +2,11 @@ package proverb
 
 import "fmt"
 
+const (
+	line = "For want of a %s the %s was lost."
+	last = "And all for the want of a %s."
+)
+
 // Proverb should return array of strings
 func Proverb(rhyme []string) []string {
 
@@ -9,10 +14,10 @@ func Proverb(rhyme []string) []string {
 
 	if len(rhyme) != 0 {
 		for i := 0; i < len(rhyme)-1; i++ {
-			result = append(result, fmt.Sprintf("For want of a %s the %s was lost.", rhyme[i], rhyme[i+1]))
+			result = append(result, fmt.Sprintf(line, rhyme[i], rhyme[i+1]))
 		}
 
-		result = append(result, fmt.Sprintf("And all for the want of a %s.", rhyme[0]))
+		result = append(result, fmt.Sprintf(last, rhyme[0]))
 	}
 
 	return result
