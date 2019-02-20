e8ecd3540b5a48154c1a5efc04cbfe6cee9780b5
Scott Sprunger <github@globaleworks.com>
2019-02-20 17:12:48 -0500

    Moving mapping outside of function. Clean up.

--- go/roman-numerals/roman_numerals.go
+++ go/roman-numerals/roman_numerals.go
@@ -9,29 +9,29 @@ type arabicToRoman struct {
 	roman  string
 }
 
+var valueMapping = [...]arabicToRoman{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
 // ToRomanNumeral returns string representation of integer [1..3000]
 func ToRomanNumeral(input int) (result string, err error) {
 	if input < 1 || input > 3000 {
 		return result, fmt.Errorf("The value %d is out of range [1..3000]", input)
 	}
 
-	mappings := []arabicToRoman{
-		{1000, "M"},
-		{900, "CM"},
-		{500, "D"},
-		{400, "CD"},
-		{100, "C"},
-		{90, "XC"},
-		{50, "L"},
-		{40, "XL"},
-		{10, "X"},
-		{9, "IX"},
-		{5, "V"},
-		{4, "IV"},
-		{1, "I"},
-	}
-
-	for _, value := range mappings {
+	for _, value := range valueMapping {
 		for input >= value.arabic {
 			result += value.roman
 			input -= value.arabic
