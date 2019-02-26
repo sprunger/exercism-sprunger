c22ef7ff90c46ab34bfd2663bfe232991335960a
Scott Sprunger <github@globaleworks.com>
2019-02-26 09:27:07 -0500

    Per mentor replaced Map with Switch and used unicode.ToLower - significant performance improvement

--- go/scrabble-score/scrabble.go
+++ go/scrabble-score/scrabble.go
@@ -1,6 +1,6 @@
 package scrabble
 
-import "strings"
+import "unicode"
 
 // Letter scores used to determine word score
 var letters = map[rune]int{
@@ -17,9 +17,23 @@ var letters = map[rune]int{
 // Score returns the sum of letter scores in the provided word
 func Score(word string) (score int) {
 
-	word = strings.ToLower(word)
 	for _, char := range word {
-		score += letters[char]
+		switch unicode.ToLower(char) {
+		case 'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't':
+			score++
+		case 'd', 'g':
+			score += 2
+		case 'b', 'c', 'm', 'p':
+			score += 3
+		case 'f', 'h', 'v', 'w', 'y':
+			score += 4
+		case 'k':
+			score += 5
+		case 'j', 'x':
+			score += 8
+		case 'q', 'z':
+			score += 10
+		}
 	}
 
 	return
