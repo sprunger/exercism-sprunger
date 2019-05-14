9dc549f5ddf6be3f8b9c4a0c88794ffef8842f2a
Scott Sprunger <github@globaleworks.com>
2019-05-14 10:06:41 -0400

    First iteration of exercise

---
+++ go/anagram/anagram.go
@@ -0,0 +1,32 @@
+package anagram
+
+import (
+	"sort"
+	"strings"
+)
+
+// SortedString returns are string with all of the characters sorted
+func SortedString(s string) string {
+	s = strings.ToLower(s)
+	chars := strings.Split(s, "")
+	sort.Strings(chars)
+	return strings.Join(chars, "")
+}
+
+// Detect possible anagrams given a word and a list of candidates
+func Detect(subject string, candidates []string) (anagrams []string) {
+	if len(subject) == 0 {
+		return
+	}
+
+	//	subject = strings.ToLower(subject)
+
+	for _, candidate := range candidates {
+		if !strings.EqualFold(subject, candidate) {
+			if SortedString(candidate) == SortedString(subject) {
+				anagrams = append(anagrams, candidate)
+			}
+		}
+	}
+	return
+}
